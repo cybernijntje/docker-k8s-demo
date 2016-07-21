e74cd49fab5e07b207173b7dae0612fa783b2b95
Dennis Nicolaas van der Meijden <dmeijden@cybernijntje.local>
2016-07-21 11:47:56 +0200

    First commit

---
+++ docker/myapp/main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'mysql-server'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'myapp'
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM location WHERE id = 2''')
+    rv = cur.fetchall()
+    return str(rv)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=80)
